7bb0ccd61509dddda333c0f3a8f328242bc4d45f
Admin <anishkdevadiga2002@gmail.com>
2024-08-29 20:32:04 +0530

    not tested

---
+++ lib/main.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Image Caption Generator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to Image Caption Generator',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(_createRoute());
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.blue,
+                backgroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => CaptionPage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+}
+
+class CaptionPage extends StatefulWidget {
+  @override
+  _CaptionPageState createState() => _CaptionPageState();
+}
+
+class _CaptionPageState extends State<CaptionPage> {
+  File? _imageFile; // Stores the selected image file
+  String? responseText;
+  bool _isLoading = false;
+
+  // Initialize the Gemini model
+  final model = GenerativeModel(
+    model: 'gemini-1.5-flash',
+    apiKey: "AIzaSyCnGLrMegmnfFKs4xzifuE_Lgonz2f9xPQ", // Replace with your actual API key
+  );
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _imageFile = File(pickedFile.path);
+      }
+    });
+  }
+
+  Future<void> _generateCaption() async {
+    if (_imageFile == null) return;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      // Assume model.generateCaption is the method to generate caption from image
+      final content = Content.image(imageFile: _imageFile!);
+      final response = await model.generateContent([content]);
+
+      setState(() {
+        responseText = response?.text;
+        _isLoading = false;
+      });
+
+      print('API Response: ${response?.text}');
+    } catch (error, stackTrace) {
+      setState(() {
+        responseText = 'Error: $error';
+        _isLoading = false;
+      });
+
+      print('Error: $error');
+      print('Stack Trace: $stackTrace');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Image Caption Generator', style: TextStyle(color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.blue),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              if (_imageFile != null)
+                Image.file(
+                  _imageFile!,
+                  height: 200,
+                ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _pickImage,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.blue,
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text('Upload Image'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _generateCaption,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.blue,
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: _isLoading
+                    ? const CircularProgressIndicator()
+                    : const Text('Generate Caption'),
+              ),
+              const SizedBox(height: 16.0),
+              if (responseText != null)
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    'Generated Caption:\n\n$responseText',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (responseText != null) {
+                    Clipboard.setData(ClipboardData(text: responseText));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Caption copied to clipboard!'),
+                      ),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.blue,
+                  backgroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text('Copy Caption'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
